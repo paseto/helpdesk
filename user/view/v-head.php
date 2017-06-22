ef31c709f9426e72b8c83820813243bb7428b1bf
Giovani Paseto <giovaniw2@gmail.com>
2017-06-22 10:42:40 -0300

    Tradução e filtro

--- user/view/v-head.php
+++ user/view/v-head.php
@@ -5,7 +5,7 @@ ob_start();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Acorn Aid</title>
+<title>Help Desk</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 <link rel="shortcut icon" href="../public/images/favicon/favicon.ico" />
